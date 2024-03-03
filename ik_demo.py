cdbae5daa47f4fb399dd885f94e19510d1f61c6f
jinyin-z19 <19807733522@163.com>
2024-03-03 12:37:45 +0800

    udpate

--- ik_demo.py
+++ ik_demo.py
@@ -1,25 +1,56 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 import sys
+
 print("Python version:", sys.version)
 
 import numpy as np
-sys.path.append('./walking_packet')
+
+sys.path.append("./walking_packet")
 from model_h_kinematics import *
 from model_h_motors_control import *
 from time import sleep
 import time
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     leg = ModelHLegIK()
-    leg_r_ang = leg.LegIKMove("right",[0, 0, -0.30, 0, 0, 0])
-    arm_r_ang = [0,-0.1,0]
-    leg_l_ang = leg.LegIKMove("left",[0, 0, -0.30, 0, 0, 0])
-    arm_l_ang = [0,0.1,0]
+    leg_r_ang = leg.LegIKMove("right", [0, 0, -0.30, 0, 0, 0])
+    arm_r_ang = [0, -0.1, 0]
+    leg_l_ang = leg.LegIKMove("left", [0, 0, -0.30, 0, 0, 0])
+    arm_l_ang = [0, 0.1, 0]
     command_list = leg_r_ang + arm_r_ang + leg_l_ang + arm_l_ang
     print(command_list)
+    # -- stand leg zero position --    
+    zero_list = [0] * 18
+    
+    # right leg
+    zero_list[0] = 0.45
+    zero_list[1] =-0.05
+    zero_list[2] = 0.00
+    zero_list[3] = 0.00
+    zero_list[4] = 0.00
+    zero_list[5] = 0.00
+    
+    # right arm
+    zero_list[6] = 0.00
+    zero_list[7] = -0.1
+    zero_list[8] = 0.00
+        
+    # left leg
+    zero_list[9] = -0.25
+    zero_list[10] = 0.05
+    zero_list[11] =-0.03
+    zero_list[12] =-0.08
+    zero_list[13] = 0.00
+    zero_list[14] = 0.00
+    
+    # left arm
+    zero_list[15] = 0.00
+    zero_list[16] = 0.10
+    zero_list[17] = 0.00
+    
+    ang_move = np.array(command_list) + np.array(zero_list)
     mctl = ModelHCtl()
-    ang_move = mctl.MotorCall()
     print(mctl.MotorCall())
-    mctl.MotorMove(ang_move)      
-
+    print(ang_move)
+    mctl.MotorSafeMove(ang_move.tolist())
--- walking_packet/model_h_motors_control.py
+++ walking_packet/model_h_motors_control.py
@@ -7,47 +7,67 @@ lib = ctypes.CDLL('./walking_packet/MODEL_H_MOTOR_BUILDER/build/lib/libliveltbot
 # Define the function argument and return types
 lib.joints_move.argtypes = [ctypes.c_float * 18]
 lib.joints_move.restype = None
-        
+
 lib.joints_state.argtypes = [ctypes.c_float * 18]
 lib.joints_state.restype = None
-               
+
 class ModelHCtl:
-    """Motor control for modle h"""
-    def __init__(self):
-        """
-        initialize class
-        """
-        # get C list pointer
-        self.motor_ang = [0] * 18
-        self.c_motor_ang = (ctypes.c_float * 18)(*self.motor_ang)
-        # set pointer
-        self.c_motor_ang_ptr = ctypes.pointer(self.c_motor_ang)   
-            
- 
-    def MotorMove(self, tar_motor_ang):
-        """
-        motors move
-        """       
-        for index in range(18):
-            self.c_motor_ang[index] = tar_motor_ang[index]
-        lib.joints_move(self.c_motor_ang)
+  """Motor control for modle h"""
+  def __init__(self):
+    """
+    initialize class
+    """
+    # get C list pointer
+    self.motor_ang = [0] * 18
+    self.motor_rec = [0] * 18
+    self.motor_ang_rec = [0] * 18
+    self.c_motor_ang = (ctypes.c_float * 18)(*self.motor_ang)
+    self.c_motor_rec = (ctypes.c_float * 18)(*self.motor_rec)
+    # set pointer
+    self.c_motor_ang_ptr = ctypes.pointer(self.c_motor_ang)
+    self.c_motor_rec_ptr = ctypes.pointer(self.c_motor_rec)
+    # max speed of one command
+    self.max_speed = 0.05   
 
 
-    def MotorCall(self):
-        """
-        motors pos callback
-        """
-        lib.joints_state(self.c_motor_ang)       
-        theta = ctypes.cast(self.c_motor_ang_ptr, ctypes.POINTER(ctypes.c_float * 18)).contents
-        for index in range(18):
-            self.motor_ang[index] = theta[index]
-        return self.motor_ang
-        
-               
+  def MotorMove(self, tar_motor_ang):
+    """
+    motors move
+    """       
+    for index in range(18):
+      self.c_motor_ang[index] = tar_motor_ang[index]
+    lib.joints_move(self.c_motor_ang)
+
+
+  def MotorCall(self):
+    """
+    motors pos callback
+    """
+    lib.joints_state(self.c_motor_rec)       
+    theta = ctypes.cast(self.c_motor_rec_ptr, ctypes.POINTER(ctypes.c_float * 18)).contents
+    for index in range(18):
+      self.motor_ang_rec[index] = theta[index]
+    return self.motor_ang_rec
+
+  def MotorSafeMove(self, tar_motor_ang):
+    """
+    motors move safely
+    """ 
+    lib.joints_state(self.c_motor_rec)       
+    theta = ctypes.cast(self.c_motor_rec_ptr, ctypes.POINTER(ctypes.c_float * 18)).contents    
+    for index in range(18):
+      if tar_motor_ang[index] - theta[index] > self.max_speed:
+        self.c_motor_ang[index] = theta[index] + self.max_speed
+      elif tar_motor_ang[index] - theta[index] < -self.max_speed:
+        self.c_motor_ang[index] = theta[index] - self.max_speed
+      else:
+        self.c_motor_ang[index] = tar_motor_ang[index]	
+    lib.joints_move(self.c_motor_ang)        
+
 if __name__ == '__main__':
-    mctl = ModelHCtl()
-    ang_move = mctl.MotorCall()
-    print(mctl.MotorCall())
-    mctl.MotorMove(ang_move)
-    
+  mctl = ModelHCtl()
+  ang_move = mctl.MotorCall()
+  print(mctl.MotorCall())
+  mctl.MotorMove(ang_move)
+
 
